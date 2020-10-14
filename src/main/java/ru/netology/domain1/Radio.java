e64b005ce29c54c78e56acbebff1339c06b4e285
Nadya Chistyakova <npodtynchenko@list.ru>
2020-10-14 23:09:38 +0300

    new

---
+++ src/main/java/ru/netology/domain1/Radio.java
@@ -0,0 +1,72 @@
+package ru.netology.domain1;
+
+public class Radio {
+    private int station;
+    private int volume;
+
+    public int getStation() {
+        return station;
+    }
+    public void setStation(int station) {
+        if (station > 9) {
+            return;
+        }
+        if (station < 0) {
+            return;
+        }
+        this.station = station;
+    }
+
+    public void nextStation() {
+        if (station == 9) {
+            this.station = 0;
+        }
+        if (station > 0) {
+            this.station++;
+        }
+    }
+
+    public void prevStation() {
+        if (station == 0) {
+            this.station = 9;
+        }
+        if (station < 9) {
+            this.station--;
+        }
+    }
+
+    public void choiceStation(int number) {
+        if (number > 9) {
+            return;
+        }
+        if (number < 0) {
+            return;
+        }
+        this.station = number;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void increaseVolume() {
+        if (volume >= 10) {
+            this.volume = 10;
+        }
+        if (volume < 10) {
+            this.volume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (volume <= 0) {
+            volume = 0;
+        }
+        if (volume > 0) {
+            this.volume--;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/domain1/RadioTest.java
@@ -0,0 +1,125 @@
+package ru.netology.domain1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTest {
+
+    @Test
+    public void station() {
+        Radio radio = new Radio();
+        radio.setStation(5);
+        assertEquals(5, radio.getStation());
+    }
+
+    @Test
+    public void stationFalse() {
+        Radio radio = new Radio();
+        radio.setStation(11);
+        assertEquals(0, radio.getStation());
+    }
+
+    @Test
+    public void stationFalse2() {
+        Radio radio = new Radio();
+        radio.setStation(-5);
+        assertEquals(0, radio.getStation());
+    }
+
+    @Test
+    public void nextStation() {
+        Radio radio = new Radio();
+        int station = 5;
+        radio.setStation(station);
+        radio.nextStation();
+        assertEquals(6,radio.getStation());
+    }
+
+    @Test
+    public void nextStation0() {
+        Radio radio = new Radio();
+        int station = 9;
+        radio.setStation(station);
+        radio.nextStation();
+        assertEquals(0,radio.getStation());
+    }
+
+    @Test
+    public void prevStation() {
+        Radio radio = new Radio();
+        int station = 5;
+        radio.setStation(station);
+        radio.prevStation();
+        assertEquals(4,radio.getStation());
+    }
+
+    @Test
+    public void prevStation9() {
+        Radio radio = new Radio();
+        int station = 0;
+        radio.setStation(station);
+        radio.prevStation();
+        assertEquals(9,radio.getStation());
+    }
+
+    @Test
+    public void choiceStation() {
+        Radio radio = new Radio();
+        int number = 7;
+        radio.choiceStation(number);
+        assertEquals(7, radio.getStation());
+    }
+
+    @Test
+    public void choiceStationFalse() {
+        Radio radio = new Radio();
+        int number = 10;
+        radio.choiceStation(number);
+        assertEquals(0, radio.getStation());
+    }
+
+    @Test
+    public void choiceStationFalse2() {
+        Radio radio = new Radio();
+        int number = -1;
+        radio.choiceStation(number);
+        assertEquals(0, radio.getStation());
+    }
+
+    @Test
+    public void increaseVolume() {
+        Radio radio = new Radio();
+        int volume = 5;
+        radio.setVolume(volume);
+        radio.increaseVolume();
+        assertEquals(6, radio.getVolume());
+    }
+
+    @Test
+    public void increaseVolumeFalse() {
+        Radio radio = new Radio();
+        int volume = 11;
+        radio.setVolume(volume);
+        radio.increaseVolume();
+        assertEquals(10, radio.getVolume());
+    }
+
+    @Test
+    public void decreaseVolume() {
+        Radio radio = new Radio();
+        int volume = 6;
+        radio.setVolume(volume);
+        radio.decreaseVolume();
+        assertEquals(5, radio.getVolume());
+    }
+
+    @Test
+    public void decreaseVolumeFalse() {
+        Radio radio = new Radio();
+        int volume = -5;
+        radio.setVolume(volume);
+        radio.decreaseVolume();
+        assertEquals(0, radio.getVolume());
+    }
+}
